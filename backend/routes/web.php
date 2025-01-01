b3d76d5b057f436c66c834f03811d629bc20cde8
Yasu1025 <sillyjp25@gmail.com>
2025-01-02 03:09:36 +0900

    [BE] login

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [AdminController::class, 'login'])->name('admin.login');
+Route::post('admin/auth', [AdminController::class, 'auth'])->name('admin.auth');
+
+Route::prefix('admin')->group(function () {
+  Route::get('dashboard', [AdminController::class, 'index'])->name('admin.index');
+  Route::post('logout', [AdminController::class, 'logout'])->name('admin.logout');
 });
